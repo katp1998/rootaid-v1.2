68a82f205f40e9d3391a14f3134a58884fdc25c5
katp98 <kethzyp@gmail.com>
2022-11-07 13:09:09 +0530

    frontend-react-ant-design: COMPLETE
    Signed-off-by: katp98 <kethzyp@gmail.com>

--- frontend-react-ant-design/src/pages/RegisterPage.tsx
+++ frontend-react-ant-design/src/pages/RegisterPage.tsx
@@ -1,8 +1,52 @@
 import React from 'react'
+import { Button, Form, Input, Card, Col, Row  } from 'antd';
 
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <div className="site-card-wrapper">
+    <Row justify="center" gutter={[16, 16]}>
+      <Col span={6} >
+        <Card title="REGISTER" bordered={true} >
+          <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            initialValues={{ remember: true }}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Name"
+              name="name"
+              rules={[{ required: true, message: 'Please input your name!' }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[{ required: true, message: 'Please input your Email!' }]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button type="primary" block danger htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+      </Col>
+    </Row>
+  </div>
   )
 }
 
