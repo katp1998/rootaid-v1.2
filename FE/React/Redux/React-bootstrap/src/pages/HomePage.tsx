06e73d7b4d8515c7b24e9f914f4c75806fa017d6
katp98 <kethzyp@gmail.com>
2022-11-14 09:41:18 +0530

    making base folder structure
    Signed-off-by: katp98 <kethzyp@gmail.com>

--- FE/React/Redux/React-bootstrap/src/pages/HomePage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function HomePage() {
-  return (
-    <div><h1>WELCOME TO THE HOMEPAGE!</h1></div>
-  )
-}
-
-export default HomePage
---
+++ root/polyrepo/FE/React/Redux/React-bootstrap/src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div><h1>WELCOME TO THE HOMEPAGE!</h1></div>
+  )
+}
+
+export default HomePage
