370a4b563728ffd574b826e4b935f8f757d7f6ab
katp98 <kethzyp@gmail.com>
2022-11-16 14:58:22 +0530

    koa-jwt-mySQL test1 code testing: COMPLETED
    Signed-off-by: katp98 <kethzyp@gmail.com>

---
+++ root/polyrepo/BE/Monolithic/REST/Koa/JWT/mySQL/dist/database/repositories/user.repository.js
@@ -0,0 +1,40 @@
+'use strict';
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+  function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+  return new (P || (P = Promise))(function (resolve, reject) {
+    function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+    function rejected(value) { try { step(generator.throw(value)); } catch (e) { reject(e); } }
+    function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+    step((generator = generator.apply(thisArg, _arguments || [])).next());
+  });
+};
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.createUser = exports.findUser = void 0;
+//koa was not used in this process
+const user_model_1 = require('../models/user.model');
+//@DESC: FIND IF USER EXISTS
+//@ROUTE: POST /api/v1.2/auth/
+const findUser = (email) => __awaiter(void 0, void 0, void 0, function* () {
+  try {
+    const existingUser = yield user_model_1.User.findOne({ where: { email } });
+    return existingUser;
+  } catch (error) {
+  }
+});
+exports.findUser = findUser;
+//@desc CREATING USER
+//@route POST /api/v1.2/auth/create
+const createUser = ({ name, email, password }) => __awaiter(void 0, void 0, void 0, function* () {
+  try {
+    //CREATE USER:
+    const user = yield user_model_1.User.save({
+      name,
+      email,
+      password,
+    });
+    return user;
+  } catch (error) {
+    return error;
+  }
+});
+exports.createUser = createUser;
