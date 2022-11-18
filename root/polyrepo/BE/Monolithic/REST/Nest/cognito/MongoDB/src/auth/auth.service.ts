1d3a07bb8af1d6604a6fcee96e9f9886b753e96d
Amrullah Farook <amrullahfahimfarook@gmail.com>
2022-11-18 11:54:51 +0530

    Added MongoDB integration & user create operation

--- root/polyrepo/BE/Monolithic/REST/Nest/cognito/MongoDB/src/auth/auth.service.ts
+++ root/polyrepo/BE/Monolithic/REST/Nest/cognito/MongoDB/src/auth/auth.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from "@nestjs/common";
 
-import * as AWS from 'aws-sdk'
-import * as crypto from 'crypto'
+import * as AWS from 'aws-sdk';
+import * as crypto from 'crypto';
+import { createUser } from '../database/repository/user.repository';
 
 @Injectable()
 export class AuthService {
 
 private config = {
-    region: 'us-east-1'
-}
+    region: 'us-east-1',
+};
 
-private secretHash = '1ofv95uvqt9knp7vsg7g3ark5k192g46qt73iur43mat30604u0c'
-private clientId = '51qtvnblrch9vm6cbhgfaj37sl'
+private secretHash = '1ofv95uvqt9knp7vsg7g3ark5k192g46qt73iur43mat30604u0c';
+private clientId = '51qtvnblrch9vm6cbhgfaj37sl';
 
-private cognitoService = new AWS.CognitoIdentityServiceProvider(this.config)
+private cognitoService = new AWS.CognitoIdentityServiceProvider(this.config);
 
 private generateHash = (username: string): string => {
     return crypto.createHmac('SHA256', this.secretHash)
@@ -27,17 +28,29 @@ async registerUser(username:string, password:string, userAttr:Array<any>) {
     Password: password,
     Username: username,
     SecretHash: this.generateHash(username),
-    UserAttributes: userAttr
+    UserAttributes: userAttr,
+  }
+
+    try {
+    const email:string = userAttr[0].Value;
+    const data = await createUser({name: username, email , password});
+    console.log(`Database entry success: username: ${username},
+    email: ${email},
+    password: ${password}
+    Log: ${data}`)
+  } catch (error) {
+    console.log(`Error occurred with database entry: ${error}`)
   }
 
   try {
-    const data = await this.cognitoService.signUp(params).promise()
+    const data = await this.cognitoService.signUp(params).promise();
     console.log(data);
-    return `Registration successful: ${username}`
+    return `Registration successful: ${username}`;
   } catch (error) {
     console.log(error);
-    return 'Registration failed'
+    return 'Registration failed';
   }
+
 }
 
 async loginUser(username:string, password:string) {
