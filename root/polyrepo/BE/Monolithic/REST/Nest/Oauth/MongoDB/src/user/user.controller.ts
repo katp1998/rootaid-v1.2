faf715076de152242854dcd33aa20b07b1f0bbcf
katp98 <kethzyp@gmail.com>
2022-11-25 13:24:31 +0530

    oauth-nestjs combination: COMPLETED
    Signed-off-by: katp98 <kethzyp@gmail.com>

--- root/polyrepo/BE/Monolithic/REST/Nest/Oauth/MongoDB/src/user/user.controller.ts
+++ root/polyrepo/BE/Monolithic/REST/Nest/Oauth/MongoDB/src/user/user.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { Protect } from 'src/utils/Guards';
 
 @Controller('user')
@@ -9,9 +10,21 @@ export class UserController {
     return { msg: 'Google Authentication in place' };
   }
 
+  //message to be displayed when the user is logged in:
   @Get('google/redirect')
   @UseGuards(Protect)
   handleRedirect() {
     return { msg: 'okay!' };
+  }
+
+  //To check if the user is serialized or not
+  @Get('status')
+  user(@Req() request: Request) {
+    console.log(request.user);
+    if (request.user) {
+      return { msg: 'Authenticated' };
+    } else {
+      return { msg: 'Unauthenticated' };
+    }
   }
 }
