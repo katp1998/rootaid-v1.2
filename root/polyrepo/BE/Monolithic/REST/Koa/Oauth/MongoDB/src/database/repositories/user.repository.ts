c68513b401cc1411df6ddf5326fcc99c7248610f
katp98 <kethzyp@gmail.com>
2022-11-28 14:33:21 +0530

    updating user.repository file
    Signed-off-by: katp98 <kethzyp@gmail.com>

--- root/polyrepo/BE/Monolithic/REST/Koa/Oauth/MongoDB/src/database/repositories/user.repository.ts
+++ root/polyrepo/BE/Monolithic/REST/Koa/Oauth/MongoDB/src/database/repositories/user.repository.ts
@@ -1,29 +1,29 @@
 //koa was not used in this process
-import { User } from "../models/user.model"
+import { User } from '../models/user.model';
 
 //@DESC: FIND IF USER EXISTS
-//@ROUTE: POST /api/v1.2/auth/
-export const findUser = async(email: string) =>{
-    try {
-        const existingUser = await User.findOne({ where: { email } })
-        return existingUser
-    } catch (error) {
-        
-    }
-}
+//@ROUTE: POST 
+export const findUser = async (email: any) =>{
+  try {
+    const existingUser = await User.findOneBy({email});
+    return existingUser;
+  } catch (error) {
+    console.log(error, 'from user.repository');
+  }
+};
 
 //@desc CREATING USER
-//@route POST /api/v1.2/auth/create
-export const createUser = async({name, email, password}: {name: string, email: string, password: string}) =>{
-try {
+//@route POST 
+export const createUser = async ({ name, email, password }: { name: string, email: string, password: string }) =>{
+  try {
     //CREATE USER:
     const user = await User.save({
-        name,
-        email,
-        password
-      })
-    return user
-} catch (error) {
-    return error
-}
-}
+      name,
+      email,
+      password,
+    });
+    return user;
+  } catch (error) {
+    return error;
+  }
+};
