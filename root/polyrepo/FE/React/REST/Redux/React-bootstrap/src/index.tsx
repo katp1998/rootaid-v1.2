f9e8b3fbe851abd9d8bbdd0ad439186b18c528b1
Mathusan <mathusanadded@gmail.com>
2022-11-29 10:00:35 +0530

    -redux-bootstrap

--- root/polyrepo/FE/React/REST/Redux/React-bootstrap/src/index.tsx
+++ root/polyrepo/FE/React/REST/Redux/React-bootstrap/src/index.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter as Router} from 'react-router-dom' 
+import './index.css';
+import './styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import App from './App';
-import './bootstrap.min.css'
+import {Provider} from 'react-redux'
+import { store } from './store/store';
 
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
 
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
+    <Router>
     <Provider store={store}>
       <App />
     </Provider>
+    </Router>
   </React.StrictMode>
 );
-
