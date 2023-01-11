a9497949ce93417c53a3ffb567cf73844268239f
Amrullah Farook <amrullahfahimfarook@gmail.com>
2023-01-11 10:27:07 +0530

    Added custom hooks to REST/Next/Redux/...

---
+++ root/polyrepo/FE/Next/REST/Redux/React-bootstrap/project/hooks/useRefreshToken.ts
@@ -0,0 +1,24 @@
+
+import { Dispatch } from 'redux';
+import axios from '../pages/api/axios';
+import { useDispatch } from 'react-redux'
+import { setAccessToken} from '../features/auth/authSlice';
+
+
+const useRefreshToken = () =>
+{
+    const dispatch: Dispatch<any> = useDispatch();
+
+    const refresh = async () => {
+        const response = await axios.get('/refreshtoken', {
+            withCredentials: true
+        });
+
+        dispatch(setAccessToken(response.data.accessToken));
+        console.log("new accesstoken", response.data.accessToken);
+        return response.data.accessToken;
+    }
+    return refresh;
+};
+
+export default useRefreshToken;
